a4e01aed3ce1631f8d635f152e1106f341f54c48
Daedalus2K <apolloswatered@gmail.com>
2023-02-15 22:28:44 -0500

    commit tree 1cb479d962c90e7a60a0e83e49957e9a1b6eaff6

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "Kratos is the champion";
+
+
 // Write your code here!
